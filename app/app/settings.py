93da7a4066516bfd2af2c5de011d926bbbe42bfa
choy1240 <shield3045@naver.com>
2024-06-20 10:45:16 +0900

    settings.py 입력

---
+++ app/app/settings.py
@@ -0,0 +1,11 @@
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+    }
+}
