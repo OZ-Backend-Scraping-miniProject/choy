ea4f648c5a0cd7c17461a54c307b47a3f9932093
choy1240 <shield3045@naver.com>
2024-06-20 15:50:07 +0900

    wait for db 수정

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,22 +1,22 @@
-import time
-
 from django.core.management.base import BaseCommand
 from django.db import connections
+import time
 
+# Operation Error & Psycopg2 Operation Error
 from django.db.utils import OperationalError
-from psycopg2 import OperationalError as Psycopg2OperationalError
+from psycopg2 import OperationalError as Psycopg2OPsycopgpError
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        self.stdout.write('Wating for DB Connection ... ')
+        self.stdout.write('Wating for DB connection ...')
 
-        is_dbconnected = None
-
+        is_db_connected = None
         while not is_db_connected:
             try:
                 is_db_connected = connections['default']
-            except(OperationalError, Psycopg2OperationalError):
-                self.stdout.write("Retry DB Connection ... ")
+            except (OperationalError, Psycopg2OPsycopgpError):
+                self.stdout.write("Retrying DB connection ...")
                 time.sleep(1)
-
-        self.stdout.write(self.style.SUCCESS('Success to PostgreSQL connection'))
+        self.stdout.write(self.style.SUCCESS("Congratue!\
+            PostgreSQL Connection Success!!!"))
